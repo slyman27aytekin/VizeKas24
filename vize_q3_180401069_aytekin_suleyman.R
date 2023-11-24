47211bc3eefd04f68c9c147c9e80ad1db9027679
93merty <123013496+93merty@users.noreply.github.com>
2023-11-24 16:41:35 +0300

    test2_12

---
+++ vize_q3_180401069_aytekin_suleyman.R
@@ -0,0 +1,28 @@
+sevilen_sanatci_id <- sapply(sevilen_sanatcilar, function(artist) {
+  result <- spotify_search_artist(artist)
+  result$search_results$id[1]  # İlk sonucun ID'sini al
+})
+
+sevilmeyen_sanatci_id <- sapply(sevilmeyen_sanatcilar, function(artist) {
+  result <- spotify_search_artist(artist)
+  result$search_results$id[1]  # İlk sonucun ID'sini al
+})
+
+my_artists <- data.frame(artist = c(sevilen_sanatcilar, sevilmeyen_sanatcilar),
+                         id = c(sevilen_sanatci_id, sevilmeyen_sanatci_id))
+
+my_artists_combined <- cbind(sevilen = sevilen_sanatci_id, sevilmeyen = sevilmeyen_sanatci_id)
+
+# Eşleşen ID'leri bulma
+matching_ids <- apply(my_artists_combined, 1, function(row) {
+  identical(row["sevilen"], row["sevilmeyen"])
+})
+
+matching_artists <- my_artists[matching_ids, ]
+non_matching_artists <- my_artists[!matching_ids, ]
+
+# Eşleşen ve eşleşmeyen sanatçıları görüntüleme
+print("Eşleşen Sanatçılar:")
+print(matching_artists)
+print("Eşleşmeyen Sanatçılar:")
+print(non_matching_artists)
