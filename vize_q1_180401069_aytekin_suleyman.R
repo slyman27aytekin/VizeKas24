e20b1a923235f9622093069b783671e966201e8c
93merty <123013496+93merty@users.noreply.github.com>
2023-11-24 16:21:30 +0300

    test_1_1

--- vize_q1_180401069_aytekin_suleyman.R
+++ vize_q1_180401069_aytekin_suleyman.R
@@ -1,35 +1,32 @@
-
 spotify_token <- function() {
   client_id <- Sys.getenv("SPOTIFY_CLIENT_ID")
   client_secret <- Sys.getenv("SPOTIFY_CLIENT_SECRET")
   
-  # Base64 encode edilmiş client ID ve client secret
+  token_url <- "https://accounts.spotify.com/api/token"
+  
   client_credentials <- base64enc::base64encode(paste0(client_id, ":", client_secret))
   
-  # HTTP POST isteği için gerekli header bilgileri
   headers <- c(
     `Content-Type` = "application/x-www-form-urlencoded",
     Authorization = paste("Basic", client_credentials)
   )
   
-  # POST isteği gönderme
   response <- httr::POST(
     url = token_url,
     add_headers(.headers = headers),
     body = list(grant_type = "client_credentials")
   )
   
-  # HTTP status kodunu alma
   status_code <- httr::status_code(response)
-  
-  # JSON formatındaki cevabı alma ve içinden token'ı çıkarma
   token <- httr::content(response)$access_token
   
-  # Döndürülecek liste
   result <- list(
     status_code = status_code,
     token = paste("Bearer", token)
   )
   
   return(result)
-}
+}# Test 1.1
+Test_1_1 <- exists("spotify_token", envir = .GlobalEnv)
+paste("Test 1.1:", Test_1_1)
+
